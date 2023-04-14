cb33b5b99477b55413a7098a39849c5400e4adf7
Corbin Brown <cjbrown019@gmail.com>
2023-04-13 20:11:52 -0500

    Add project files.

---
+++ SAT_APP_PROJECT/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SAT_APP_PROJECT.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
